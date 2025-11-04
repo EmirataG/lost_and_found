9d315d6c0e38c9e65d91b2086827a7e17a01e4a8
EmirataG <emirahmed9.7.5@gmail.com>
2025-11-03 21:49:30 -0500

    deleted stuff

--- app/page.tsx
+++ app/page.tsx
@@ -10,24 +10,9 @@ const Page = async () => {
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
 
-  const { data } = await supabase
-    .from("suggestions")
-    .select()
-    .overrideTypes<Array<Suggestion>, { merge: false }>();
-
   return (
     <div className="p-4 min-h-screen bg-blue-100">
-      <header className="text-center mb-4 p-4">
-        <PostForm />
-        <h2 className="text-2xl font-semibold">
-          Thoughts on Next.js + Supabase + Tailwind?
-        </h2>
-        <p className="text-center">
-          Also, let's meet this week and plan doing some work on the project
-          over the break!
-        </p>
-      </header>
-      <SuggestionsList suggestions={data ? data : []} />
+      <PostForm />
     </div>
   );
 };
