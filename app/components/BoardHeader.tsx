854114e7e598ff23582b4ba886090e0ef33f7f46
EmirataG <emirahmed9.7.5@gmail.com>
2025-11-07 21:10:48 -0500

    minor ui changes

--- app/components/BoardHeader.tsx
+++ app/components/BoardHeader.tsx
@@ -1,7 +1,7 @@
 const BoardHeader = () => {
   return (
     <header className="shadow-lg sticky top-0 bg-white p-4 mb-6">
-      <h1 className="text-3xl font-semibold">
+      <h1 className="text-3xl font-semibold text-center">
         Lost @ <span className="text-yaleBlue font-bold">Yale</span>
       </h1>
     </header>
